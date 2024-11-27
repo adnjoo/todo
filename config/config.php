39686b9139c827fb8d86f8f30f768e6fc84e7ba8
Andrew Njoo <adnjoo@gmail.com>
2024-11-27 10:43:30 -0600

    vlucas/phpdotenv

--- config/config.php
+++ config/config.php
@@ -1,8 +1,16 @@
 <?php
-$host = 'localhost';
-$dbname = 'todo_app';
-$username = 'root';
-$password = '';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+// Load the .env file
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
@@ -10,5 +18,3 @@ try {
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
-?>
-
