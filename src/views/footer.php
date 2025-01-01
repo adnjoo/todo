c8dc62e9cb2c869b0738bda7be9c58d772e6cdcb
adnjoo <adnjoo@gmail.com>
2025-01-01 14:58:48 -0600

    -bs js

--- src/views/footer.php
+++ src/views/footer.php
@@ -11,6 +11,5 @@ include_once __DIR__ . '/../functions.php';
         </a>
     </p>
 </footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
