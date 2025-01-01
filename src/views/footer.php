907680b80cd11c736b0bbccf10d05b345f794f7c
adnjoo <adnjoo@gmail.com>
2025-01-01 14:55:16 -0600

    g

--- src/views/footer.php
+++ src/views/footer.php
@@ -5,6 +5,11 @@ include_once __DIR__ . '/../functions.php';
 
 <footer class="bg-light text-center py-3 mt-5">
     <p>&copy;<?= date('Y') ?> <?= htmlspecialchars($config['app_name']) ?>. All Rights Reserved.</p>
+    <p>
+        <a href="https://github.com/adnjoo/todo" target="_blank" rel="noopener noreferrer" class="text-decoration-none hover:underline">
+            View on GitHub
+        </a>
+    </p>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
